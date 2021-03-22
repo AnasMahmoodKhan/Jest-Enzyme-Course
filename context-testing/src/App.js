7aab1e1b8d3f4aec58879a9a7b46cb0d27b2bb28
AnasMahmoodKhan <77839562+AnasMahmoodKhan@users.noreply.github.com>
2021-03-22 20:51:30 +0530

    Add failing test to app component

--- context-testing/src/App.js
+++ context-testing/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
 
 function App() {
-  return <div/>
+  return <div data-test="component-app"></div>;
 }
 
 export default App;

5313a3ca045016c1d9246ce40e0f8780dba30333
AnasMahmoodKhan <77839562+AnasMahmoodKhan@users.noreply.github.com>
2021-03-22 22:02:58 +0530

    Add update state with value of input box upon change test

--- context-testing/src/Input.js
+++ context-testing/src/Input.js
@@ -2,7 +2,30 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Input = ({ secretWord }) => {
-  return <div data-test="component-input"></div>;
+  const [currentGuess, setCurrentGuess] = React.useState("");
+  return (
+    <div data-test="component-input">
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          type="text"
+          value={currentGuess}
+          onChange={(evt) => setCurrentGuess(evt.target.value)}
+          placeholder="enter guess"
+        />
+        <button
+          data-test="submit-button"
+          onClick={(evt) => {
+            evt.preventDefault();
+          }}
+          className="btn btn-primary mb-2"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 Input.propTypes = {

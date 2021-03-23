ec618ca4d06aafec7ee5a79c6f5073c292530b79
AnasMahmoodKhan <77839562+AnasMahmoodKhan@users.noreply.github.com>
2021-03-23 14:29:47 +0530

    Add test and setup for secretWord in App using useReducer

--- context-testing/src/App.js
+++ context-testing/src/App.js
@@ -1,8 +1,30 @@
 import React from "react";
+import hookActions from "./actions/hookActions";
 
 import "./App.css";
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "setSecretWord":
+      return { ...state, secretWord: action.payload };
+
+    default:
+      throw new Error(`Invalid action Type : ${action.type}`);
+  }
+}
+
 function App() {
+  const [state, dispatch] = React.useReducer(reducer, {
+    secretWord: null,
+  });
+
+  const setSecretWord = (secretWord) =>
+    dispatch({ type: "setSecretWord", payload: secretWord });
+
+  React.useEffect(() => {
+    hookActions.getSecretWord(setSecretWord);
+  }, []);
+
   return <div data-test="component-app"></div>;
 }
 
